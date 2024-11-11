d9d01f2a70a58e26b734016a2c804c80c72f9c2f
aaochieng <ochiengaoron@outlook.com>
2024-11-11 15:06:01 +0300

    add: section 2

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Section1 from "./components/sect1";
+import Section2 from "./components/section2";
 
 export default function Home() {
   return (
-    <div>
+    <div className="lg:flex lg:justify-between lg:gap-4">
       <Section1 />
+      <Section2/>
     </div>
   )
 }
