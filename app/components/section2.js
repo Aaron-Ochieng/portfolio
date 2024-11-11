7269285cd2fa8ff5ee6b6d5d2c9321360feb65b3
aaochieng <ochiengaoron@outlook.com>
2024-11-11 15:30:34 +0300

    add: font family & into metadata

--- app/components/section2.js
+++ app/components/section2.js
@@ -1,15 +1,21 @@
+import { intro } from "../data/intro";
+
 export default function Section2() {
+    var txt = "{txt}"
     return (
         <section className="pt-24 lg:w-1/2 lg:py-24">
             <div className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" id="#about">
 
                 {/* Hidden About  section in larger devices */}
-                <div class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                     <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
                 </div>
 
-                
-                <p className="text-blue-700 text-4xl">Section2</p>
+                <div>
+                    <p className="mb-4 font-[family-name:var(--font-geist-mono)]">
+                        {intro.intro}
+                    </p>
+                </div>
             </div>
         </section>
     )
