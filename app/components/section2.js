0e2216c4d9cf8ae8f35a5f4c610c39edd7e56e28
aaochieng <ochiengaoron@outlook.com>
2024-11-11 15:32:01 +0300

    fix: hidden about page styling

--- app/components/section2.js
+++ app/components/section2.js
@@ -7,8 +7,8 @@ export default function Section2() {
             <div className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" id="#about">
 
                 {/* Hidden About  section in larger devices */}
-                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-                    <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
+                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen dark:bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                    <h2 className="text-sm font-bold uppercase tracking-widest text-slate-600 dark:text-slate-200 lg:sr-only">About</h2>
                 </div>
 
                 <div>
