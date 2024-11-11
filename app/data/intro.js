6c0d1253c2b379aca18c2428e93dfa8c41083d6c
aaochieng <ochiengaoron@outlook.com>
2024-11-11 15:28:48 +0300

    metadata

---
+++ app/data/intro.js
@@ -0,0 +1,8 @@
+export const intro = {
+    intro: `I am a Full Stack Web and Mobile developer from Kenya.
+    I have a passion in creating seamless,beautiful and creative websites. 
+    I pay close attention to detail and strive for excellence in every design I create. 
+    I have experience with various programming languages specifically web technologies.
+    Life ia a continous and learning process so work makes me more rigorous. 
+    Im always learning to keep up with the latest web and mobile development practices to stay ahead of the curve.`
+};
