5074ae3097c3128efadbccaa029e8b60bd59b6fb
aaochieng <ochiengaoron@outlook.com>
2024-11-12 14:59:28 +0300

    add: projects

--- app/data/intro.js
+++ app/data/intro.js
@@ -33,8 +33,47 @@ export const work = {
         I also did capturing of data (sun system) to the server and report preparation in the Finance Department - ICT Section
         `,
         pos: "I.T Attachee",
-        technology: ["Computer Networking", "Software Update","Data capture"],
+        technology: ["Computer Networking", "Software Update", "Data capture"],
         date: "October, 2021 - December, 2021",
         company: "Chemelil Sugar Company"
+    }
+}
+
+export const projects = {
+    project1: {
+        name: "Lem-In",
+        description: "A golang project terminal application to deeepen my understanding of Algorithms. It uses BFS (Breadth First Search) Algorithm to find faster optimal path to place the ants without collusion.",
+        languages: ["golang"],
+        url: "https://github.com/Aaron-Ochieng/lem-in.git"
+    },
+    project2: {
+        name: "Geolocalization",
+        description: "",
+        languages: ["golang", "html", "js"],
+        url: "https://github.com/Aaron-Ochieng/go-geolocalization.git"
+    },
+    project3: {
+        name: "Flask-Mongodb",
+        description: "A Web project written in python using flask framework & MongoDB as its database. This project was to test my ability on data handling, authentication & FrontEnd-BackEnd MongoDB configuration.",
+        languages: ["python", "mongodb"],
+        url: "https://github.com/Aaron-Ochieng/Flaskblog-Mongodb.git"
+    },
+    project4: {
+        name: "Food-App",
+        description: "A UI design of a food app. The project was written to deepen my understanding of Flutter, a dart framework used to develop Mobile, Web & Desktop applications",
+        languages: ["flutter"],
+        url: "https://github.com/Aaron-Ochieng/food_app.git"
+    },
+    project5: {
+        name: "Guess It",
+        description: "A golang project to test mathematical skills & knowledge in statistics. The project is used to predict the next number range from reading a text file | giving it a number from the terminal",
+        languages: ["golang"],
+        url: "https://github.com/Aaron-Ochieng/guess-it-1.git"
+    },
+    project7: {
+        name: "NetCat",
+        description: "A recreation of netcat server functionality.Its a terminal app written in golang & It behaves like a telegram app. The app allows a max number of conections of 10 in the server, message formatting, not sending of empty messages ...etc",
+        languages: ["golang"],
+        url: "https://github.com/Aaron-Ochieng/net-cat.git"
     }
 }
