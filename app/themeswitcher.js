a755e113a411245d14f3422bbe2e9fe51e98a8f2
aaochieng <ochiengaoron@outlook.com>
2024-11-12 12:20:15 +0300

    add: theme component

---
+++ app/themeswitcher.js
@@ -0,0 +1,25 @@
+'use client'
+
+import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
+import { PiSunDimLight,PiMoonThin } from "react-icons/pi";
+
+
+
+export default function ThemeSwitcher() {
+    const [mounted, setMounted] = useState(false);
+    const { setTheme, resolvedTheme } = useTheme();
+
+    useEffect(() => { setMounted(true) }, [])
+
+    return (
+        <div>
+            {!mounted ? <></> : <div>{resolvedTheme === "light" ? <PiMoonThin onClick={() => setTheme("dark")} className="scale-150" /> : <PiSunDimLight onClick={() => setTheme("light")} className="scale-150 text-white" />}</div>}
+        </div>
+    )
+
+
+
+
+
+}
