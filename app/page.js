fedb7460dd08a02ff72f9233f9d772c5c82b53b3
aaochieng <ochiengaoron@outlook.com>
2024-11-12 12:19:48 +0300

    add:theme switcher component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Section1 from "./components/sect1";
 import Section2 from "./components/section2";
+import ThemeSwitcher from "./themeswitcher";
 
 export default function Home() {
   return (
     <div className="lg:flex lg:justify-between lg:gap-4">
+      <ThemeSwitcher/>
       <Section1 />
       <Section2/>
     </div>
