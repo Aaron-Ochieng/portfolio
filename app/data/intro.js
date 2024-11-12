ac833020b4c83ff575ab9934e533c19f1c41f8ef
aaochieng <ochiengaoron@outlook.com>
2024-11-12 11:28:49 +0300

    add: work metadata

--- app/data/intro.js
+++ app/data/intro.js
@@ -6,3 +6,24 @@ export const intro = {
     Life ia a continous and learning process so work makes me more rigorous. 
     Im always learning to keep up with the latest web and mobile development practices to stay ahead of the curve.`
 };
+
+export const education = {
+    campus: "During my time in campus, I pursued a degreen in Information and Communication Technology. I engaged in diverse range of coursework,delving into subjects such as Programming, Software development, Database Management and Networking. My academic journey equipped me with practical skills in creating innovative solutions and implimenting techologies. "
+}
+
+export const work = {
+    work_2: {
+        desc: "",
+        pos: "Apprentice Developer",
+        technology: ["golang", "javascript", "shell"],
+        date: "April, 2024 - Present",
+        company: "Zone 01 Kisumu"
+    },
+    work_1: {
+        desc: "One month of intensive coding to access my skills for the selection process into the Zone01 Kisumu 2 year program. ",
+        pos: "Piscine Candidate",
+        technology: ["golang", "shell"],
+        date: "October, 2023 - November, 2023 ",
+        company: "Zone 01 Kisumu"
+    }
+}
