d57c3348e83f1c7f365c0ff90ef0dd80f2775694
aaochieng <ochiengaoron@outlook.com>
2024-11-12 16:15:25 +0300

    fix: small devices heading section

--- app/components/section2.js
+++ app/components/section2.js
@@ -19,20 +19,46 @@ export default function Section2() {
                 </div>
             </div>
 
+
             {/* Work Experience */}
-            {Object.entries(work).map(([key, { desc, pos, technology, date, company }]) => (
-                <ol key={key}>
-                    <Card key={key} date={date} position={pos} company={company} description={desc} technology={technology} />
-                </ol>
-            ))}
+            <div className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" id="#experience">
+
+                {/* Hidden About  section in larger devices */}
+                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen dark:bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                    <h2 className="text-sm font-bold uppercase tracking-widest text-slate-600 dark:text-slate-200 lg:sr-only">Experience</h2>
+                </div>
+
+                <div>
+                    <p className="mb-4 font-[family-name:var(--font-geist-mono)] text-gray-600 dark:text-gray-300">
+                        {Object.entries(work).map(([key, { desc, pos, technology, date, company }]) => (
+                            <ol key={key}>
+                                <Card key={key} date={date} position={pos} company={company} description={desc} technology={technology} />
+                            </ol>
+                        ))}
+                    </p>
+                </div>
+            </div>
 
 
             {/* Projects */}
-            {Object.entries(projects).map(([key, { name, description, languages, url }]) => (
-                <ol key={key}>
-                    <ProjectCard name={name} description={description} languages={languages} url={url} />
-                </ol>
-            ))}
+            <div className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" id="#projects">
+
+                {/* Hidden About  section in larger devices */}
+                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen dark:bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                    <h2 className="text-sm font-bold uppercase tracking-widest text-slate-600 dark:text-slate-200 lg:sr-only">Projects</h2>
+                </div>
+
+                <div>
+                    <p className="mb-4 font-[family-name:var(--font-geist-mono)] text-gray-600 dark:text-gray-300">
+                        {Object.entries(projects).map(([key, { name, description, languages, url }]) => (
+                            <ol key={key}>
+                                <ProjectCard name={name} description={description} languages={languages} url={url} />
+                            </ol>
+                        ))}
+                    </p>
+                </div>
+            </div>
+
 
         </section>
     )
