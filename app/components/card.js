954ca1a9d4a93d92efc18d29ab7f954c3a485065
aaochieng <ochiengaoron@outlook.com>
2024-11-13 09:59:31 +0300

    fix: template formatting

--- app/components/card.js
+++ app/components/card.js
@@ -1,21 +1,34 @@
-export default function Card({ date, position, company, technology, description }) {
+import Link from "next/link";
+
+export default function Card({ date, position, company, technology, description, url }) {
     return (
         <section id="experience">
             <li className="mb-12">
                 <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                    <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block ">
+                    <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block">
                     </div>
                     <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label={date}>{date}</header>
                     <div className="z-10 sm:col-span-6">
                         <h3 className="font-medium leading-snug text-slate-200">
                             <div>
-                                <a className="inline-flex items-baseline font-medium leading-tight text-slate-600 dark:text-slate-200 group/link text-base" target="_blank" rel="noreferrer noopener">
-                                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block">
-                                    </span>
-                                    <span className="text-xl">
-                                        {company}
-                                    </span>
-                                </a>
+                                {url !== "" ? <div>
+                                    <Link href={url} className="inline-flex items-baseline font-medium leading-tight text-slate-600 dark:text-slate-200 group/link text-base" target="_blank" rel="noreferrer noopener">
+                                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block">
+                                        </span>
+                                        <span className="text-xl">
+                                            {company}
+                                        </span>
+                                    </Link>
+                                </div> : <div>
+                                    <div className="inline-flex items-baseline font-medium leading-tight text-slate-600 dark:text-slate-200 group/link text-base" target="_blank" rel="noreferrer noopener">
+                                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block">
+                                        </span>
+                                        <span className="text-xl">
+                                            {company}
+                                        </span>
+                                    </div>
+                                </div>
+                                }
                             </div>
                             <div>
                                 <div className="text-slate-500" aria-hidden="true">{position}</div>
