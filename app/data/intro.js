da4e0db8416258c036a8a12f84777eb8c6b20d12
aaochieng <ochiengaoron@outlook.com>
2024-11-13 12:27:50 +0300

    fix: app data programming languages

--- app/data/intro.js
+++ app/data/intro.js
@@ -46,37 +46,37 @@ export const projects = {
     project1: {
         name: "Lem-In",
         description: "A golang project terminal application to deeepen my understanding of Algorithms. It uses BFS (Breadth First Search) Algorithm to find faster optimal path to place the ants without collusion.",
-        languages: ["golang"],
+        languages: ["Golang"],
         url: "https://github.com/Aaron-Ochieng/lem-in.git"
     },
     project2: {
         name: "Geolocalization",
         description: "A web project testing on api usage & displaying of maps on a webpage. The project generates coordinates (longitudes & latitude) based on a given location name & placing the markers on a map.",
-        languages: ["golang", "html", "js"],
+        languages: ["Golang", "HTML", "Javascript"],
         url: "https://github.com/Aaron-Ochieng/go-geolocalization.git"
     },
     project3: {
         name: "Flask-Mongodb",
         description: "A Web project written in python using flask framework & MongoDB as its database. This project was to test my ability on data handling, authentication & FrontEnd-BackEnd MongoDB configuration.",
-        languages: ["python", "mongodb"],
+        languages: ["Python", "MongoDB"],
         url: "https://github.com/Aaron-Ochieng/Flaskblog-Mongodb.git"
     },
     project4: {
         name: "Food-App",
         description: "A UI design of a food app. The project was written to deepen my understanding of Flutter, a dart framework used to develop Mobile, Web & Desktop applications",
-        languages: ["flutter"],
+        languages: ["Flutter"],
         url: "https://github.com/Aaron-Ochieng/food_app.git"
     },
     project5: {
         name: "Guess It",
         description: "A golang project to test mathematical skills & knowledge in statistics. The project is used to predict the next number range from reading a text file | giving it a number from the terminal",
-        languages: ["golang"],
+        languages: ["Golang"],
         url: "https://github.com/Aaron-Ochieng/guess-it-1.git"
     },
     project7: {
         name: "Net Cat",
         description: "A recreation of netcat server functionality.Its a terminal app written in golang & It behaves like a telegram app. The app allows a max number of conections of 10 in the server, message formatting, not sending of empty messages ...etc",
-        languages: ["golang"],
+        languages: ["Golang"],
         url: "https://github.com/Aaron-Ochieng/net-cat.git"
     },
     project8: {
